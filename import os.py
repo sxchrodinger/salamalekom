0d8980e6920b29919196fd84c1bf808ad8ea6d2f
Zulfahmi M. Ardianto <zulfahmimaulana3@gmail.com>
2024-05-30 23:55:24 +0700

    commit 1-0

--- import os.py
+++ import os.py
@@ -1,30 +1,13 @@
 import os
 from random import randint
-from datetime import datetime, timedelta
 
-# Get the current date
-current_date = datetime.now()
-
-# Iterate over each day of the current year
-for day in range(1, current_date.timetuple().tm_yday + 1):
+for day in range(1, 366):  # Mengubah 365 menjadi 366 untuk mewakili tahun 2024
     
-    # Calculate the date for this day
-    commit_date = current_date - timedelta(days=day)
-    
-    # Iterate over a random number of commits for this day
     for commit in range(0, randint(1, 10)):
-        # Format the date for Git commit
-        d = commit_date.strftime('%a %b %d %X %Y %z')
-        
-        # Write to file
+        d = str(day) + ' days ago'
         with open('file.txt', 'a') as file:
             file.write(d + '\n')
-        
-        # Stage changes
         os.system('git add .')
-        
-        # Commit changes with the specified date
-        os.system(f'GIT_AUTHOR_DATE="{d}" GIT_COMMITTER_DATE="{d}" git commit -m "commit {day}-{commit}"')
+        os.system(f'git commit --date="{d}" -m "commit {day}-{commit}"')
 
-# Push changes to the remote repository
 os.system('git push -u origin main')
